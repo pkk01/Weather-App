06e090950057df340ee2daf81d353e9a10a38ef1
Pratham Kumar <prk61dlw@gmail.com>
2024-08-30 19:54:26 +0530

    added search icon, cloud image and some text

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -1,7 +1,15 @@
+import java.awt.Cursor;
 import java.awt.Font;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class WeatherAppGui extends JFrame {
     public WeatherAppGui() {
@@ -24,13 +32,67 @@ public class WeatherAppGui extends JFrame {
 
     private void addGuiComponents() {
         JTextField searchTextField = new JTextField();
+        // set the location and size of the component
         searchTextField.setBounds(15, 15, 351, 45);
 
         // changing font styles and size
         searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
         add(searchTextField);
+
+        // search button
+        JButton searchButton = new JButton(loadImage("src\\assets\\search.png"));
+
+        // change cursor to a hand when hovering over the button
+
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+
+        // weather image
+        JLabel weatherConditionImage = new JLabel(loadImage("src\\assets\\cloudy.png"));
+        weatherConditionImage.setBounds(0, 125, 450, 217);
+        add(weatherConditionImage);
+
+        // temperature text
+
+        JLabel temperatureText = new JLabel("10 C");
+        temperatureText.setBounds(0, 350, 450, 45);
+        temperatureText.setFont(new Font("Dialog", Font.BOLD, 48));
+
+        // center the text
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
+
+        // weather condition description
+        JLabel weatherConditionDesc = new JLabel("Cloudy");
+        weatherConditionDesc.setBounds(0, 405, 450, 36);
+        weatherConditionDesc.setFont(new Font("Dialog", Font.PLAIN, 32));
+        weatherConditionDesc.setHorizontalAlignment(SwingConstants.CENTER);
+        add(weatherConditionDesc);
+
+        // adding humidity image
+        JLabel humidityImage = new JLabel(loadImage("src\\assets\\humidity.png"));
+        humidityImage.setBounds(15, 500, 74, 66);
+        add(humidityImage);
+
+        // humidity text
+
+        JLabel humidityText = new JLabel("<html><b>Humidity</b> 100% </html>"); // we can html in JLabel
+        humidityText.setBounds(90, 500, 85, 55);
+        humidityText.setFont(new Font("Dialog", Font.PLAIN, 16));
     }
 
-    // set the location and size of the component
+    private ImageIcon loadImage(String resourcePath) {
+        try {
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            return new ImageIcon(image);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+        System.out.println("Could not find resource");
+        return null;
+    }
 
 }
